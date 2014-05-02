ee46b56578845702d2164f3a7948267c1e87ec0f
Teodor Halvadzhiev <thalvadzhiev@gmail.com>
2014-05-02 23:47:46 +0300

    basic batarang functionality

--- src/Batarangs.py
+++ src/Batarangs.py
@@ -2,110 +2,63 @@ import math
 import sys
 import pygame
 
-from VectorMath import seperate_point
-
-from pygame.math import Vector2 as Vector
-
-from collections import OrderedDict
-from _collections import defaultdict
-from itertools import combinations
 
 
 pygame.init()
 
-screen = pygame.display.set_mode((500, 500))
+screen = pygame.display.set_mode((1000, 500))
 
 clock = pygame.time.Clock()
 
+class Batarang(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        #=======================================================================
+        # self.imageMaster = pygame.Surface((50,100))
+        # self.imageMaster.fill((255,0,0))
+        #=======================================================================
+        self.imageMaster = pygame.image.load("batman.png")
+        self.imageMaster = self.imageMaster.convert()
+        self.imageMaster = pygame.transform.scale(self.imageMaster, (50, 100))
+        #self.imageMaster = self.imageMaster.convert()
+        self.image = self.imageMaster
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey((255, 255, 255))
+        self.rect.center = (320, 240)
+        self.dir = 10
+        self.gravity = 0
+        self.frames = 0
+        
+    def rotate(self):
+        oldCenter = self.rect.center
+        self.image = pygame.transform.rotate(self.imageMaster, self.dir)
+        self.rect = self.image.get_rect()
+        self.rect.center = oldCenter
+        self.dir += 40
+        if self.dir > 360:
+            self.dir = 40
 
-class Triangle:
 
-    def __init__(self, edges, position_m=Vector((0, 0))):
-        self.diraction = (Vector((1, 0)), Vector((0, 1)))
-        self.position_m = position_m
-        self.edge_lenghts = dict(zip(["AB", "AC", "BC"], edges))
-        self.calculate_vertices()
+    def move(self, orientation):
+        #orientation 1 or -1
+        #introduce second parameter to implement gravity
+        self.frames += 1
+        #after some time gravity starts to take effect
+        if(self.frames >= 15):
+            self.gravity += 0.5
+        self.rect = self.rect.move([20 * orientation, self.gravity])
 
-  #  def __init__(self, vertices, lol, position_m=Vector((0, 0))):
-  #      self.diraction = (Vector((1, 0)), Vector((0, 1)))
-  #      self.position_m = position_m
-  #      self.vertices = dict(zip(["A", "B", "C"], vertices))
-  #      self.calculate_edges()
-
-    def calculate_edges(self):
-        self.edge_lenghts = {sorted(pair[0] + pair[1]): (
-            self.vertices[pair[0]] - self.vertices[pair[1]]).length()
-            for pair in combinations(self.vertices.keys(), 2)}
-
-    def sinc_vertices_with_median(self):
-        current_centroid = sum(self.vertices.values(), Vector((0, 0))) / 3
-        translation_vector = self.position_m - current_centroid
-        self.vertices = {key: self.vertices[key] +
-                         translation_vector for key in self.vertices.keys()}
-
-    def calculate_vertices(self):
-        self.vertices = {
-            "A": Vector((0, 0)), "B": Vector((self.edge_lenghts["AB"], 0))}
-        x_c = (self.edge_lenghts["AB"] ** 2 + self.edge_lenghts["AC"] ** 2 -
-               self.edge_lenghts["BC"] ** 2) / (2 * self.edge_lenghts["AB"])
-        y_c = (self.edge_lenghts["AB"] ** 2 - x_c ** 2) ** 0.5
-        self.vertices["C"] = Vector((x_c, y_c))
-        self.sinc_vertices_with_median()
-
-    def rotate(self, rotation):
-        for key in self.vertices:
-            self.vertices[key] = (self.vertices[key] - self.position_m).rotate(
-                rotation) + self.position_m
-
-    def move(self, movement_m):
-        self.position_m = self.position_m + movement_m
-        self.vertices = {key: self.vertices[key] + movement_m
-                         for key in self.vertices.keys()}
-
-    def draw(self):
-        pygame.draw.polygon(screen, (0, 0, 0), list((_.x, _.y)
-                            for _ in self.vertices.values()))
-   # def collide_triangle(self, triangle):
-   #     if
-
-triangle = Triangle([50, 50, 50], (25, 250))
-
+a = Batarang()
+allSprites = pygame.sprite.Group(a)
 while True:
-    keys = pygame.key.get_pressed()
-
-    if keys[pygame.K_LEFT]:
-        triangle.move(Vector((1, 0)))
-
-        screen.fill((55, 155, 255))
-        triangle.draw()
-
-    elif keys[pygame.K_RIGHT]:
-        triangle.move(Vector((-1, 0)))
-
-        screen.fill((55, 155, 255))
-        triangle.draw()
-
-    elif keys[pygame.K_UP]:
-        triangle.rotate(1)
-
-        screen.fill((55, 155, 255))
-        triangle.draw()
-
-    elif keys[pygame.K_DOWN]:
-        triangle.rotate(-1)
-
-        screen.fill((55, 155, 255))
-        triangle.draw()
-
-    else:
-        screen.fill((55, 155, 255))
-        triangle.draw()
-
-    # pygame.draw.polygon(screen, (255, 255, 255),
-    #                    [(0, 0), (0, 50), (50, 50), (50, 0)])
-    pygame.display.update()
-    clock.tick(60)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+    screen.fill((255, 255, 255))
+    screen.blit(a.image, a.rect)
+    a.move(1)
+    a.rotate()
+    pygame.display.update()
+    clock.tick(30)
+
+
