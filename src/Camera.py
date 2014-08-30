1dee224bda218ba38ecc7a6452a176b2f54afe59
thalvadzhi <thalvadzhiev@gmail.com>
2014-08-30 15:18:29 +0300

    now works with more types

--- src/Camera.py
+++ src/Camera.py
@@ -1,11 +1,11 @@
 from pygame import *
-
+from pygame.math import Vector2 as Vector
 
 class Camera:
     def __init__(self, width, height, window_width, window_height):
         self.state = Rect(0, 0, width, height)
-        self.half_width = width // 2
-        self.half_height = height // 2
+        self.half_width = window_width // 2
+        self.half_height = window_height // 2
         self.window_width = window_width
         self.window_height = window_height
 
@@ -17,13 +17,34 @@ class Camera:
     def apply_to_rect(self, target):
         return target.move(self.state.topleft)
 
+    def apply_to_tuple(self, target):
+        result_x = target[0] + self.state.x
+        result_y = target[1] + self.state.y
+        return result_x, result_y
+
+    def reverse_apply(self, target):
+        result_x = target[0] - self.state.x
+        result_y = target[1] - self.state.y
+        return result_x, result_y
+
+    def apply_to_vertices(self, vertices):
+        for vertex in vertices:
+            vertex.x += self.state.x
+            vertex.y += self.state.y
+        return vertices
+
+    def apply_to_vertex(self, vertex):
+        return Vector(vertex.x + self.state.x, vertex.y + self.state.y)
+
+
     #this updates sprites
     def update(self, target):
         self.state = self.functionality(target.rect)
 
     #this updates rects
     def update_rect(self, target):
-        self.state = self.functionality(target)
+        self.state = self.functionality((target.x, target.y))
+
 
     def functionality(self, target):
         left, top = target[0], target[1]
