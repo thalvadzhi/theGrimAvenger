113f4681b3de9941bce67b59832b0f8279db4387
thalvadzhi <thalvadzhiev@gmail.com>
2014-08-30 15:26:30 +0300

    Changes and stuff

--- src/Batarangs.py
+++ src/Batarangs.py
@@ -2,45 +2,57 @@ import math
 import sys
 import pygame
 import Camera
-import Environment
+#import Environment
 from pygame.math import Vector2 as Vector
+from BasicShapes import Rectangle
+from pixelperfect import get_hitmask
 
-
-class Batarang(pygame.sprite.Sprite):
+class Batarang():
     def __init__(self, x, y):
         #x and y are the coordinates of player's hand
-        pygame.sprite.Sprite.__init__(self)
+        self.width = 60
+        self.height = 30
         self.image_master = pygame.image.load("batarang.png").convert_alpha()
-        self.image_master = pygame.transform.scale(self.image_master, (60, 30))
+        self.image_master = pygame.transform.scale(self.image_master, (self.width, self.height))
         self.image = self.image_master
-        self.rect = self.image.get_rect()
-        self.rect.center = (x, y)
+        self.rect_center = (x, y)
+        self.rect = Rectangle.get_rect(self.image, self.rect_center)
+
+        #coordinates of topleft vertex
+        self.x = x - self.width // 2
+        self.y = y - self.height // 2
+
+        self.mask = get_hitmask(self, 0)
+
         self.rotation = 0
         self.gravity = 0
         self.frames = 0
         self.step = 40
-        self.x = x
-        self.y = y
         self.should_fly = False
-        
-    def rotate(self):
-        old_center = self.rect.center
-        self.image = pygame.transform.rotate(self.image_master, self.rotation)
-        self.rect = self.image.get_rect()
-        self.rect.center = old_center
+
+
+    def rotate(self, timer):
+        self.image = pygame.transform.rotate(self.image_master, self.rotation * (1000 / timer))
+        self.rect = Rectangle.get_rect(self.image, self.rect_center)
+        self.mask = get_hitmask(self, 0)
         self.rotation += self.step
         if self.rotation > 360:
             self.rotation = self.step
         if self.frames >= 25:
             self.step -= 0.7
-        
-    def move(self):
-        self.frames += 1
-        #after some time gravity starts to take effect
-        if self.frames >= 25:
-            self.gravity += 0.4
-        self.rect = self.rect.move([self.direction.x * 20, self.direction.y * 20 + self.gravity])
-        self.rotate()
+
+
+    def move(self, timer):
+        #IMPLEMENT GRAVITY
+        speed = 25 * 60 * (timer / 1000)
+        self.x += self.direction.x * speed
+        self.y += self.direction.y * speed
+
+        old_center = self.rect_center
+
+        self.rect_center = (old_center[0] + self.direction.x * speed, old_center[1] + self.direction.y * speed)
+
+        self.rotate(timer)
         
     
     def direct(self, mouse_x, mouse_y):
@@ -55,10 +67,13 @@ class Batarang(pygame.sprite.Sprite):
         self.direct(mouse_position[0], mouse_position[1])
         self.should_fly = True
 
-    def draw(self, surface, camera):
-        surface.blit(self.image, camera.apply_to_rect(self.rect))
+    def update(self, timer):
         if self.should_fly:
-            self.move()
-            self.rotate()
+            self.move(timer)
+            self.rotate(timer)
 
+    def draw(self, surface, camera):
+        #to be moved in Render module
+        surface.blit(self.image, camera.apply_to_tuple((self.x, self.y)))
+        self.rect.draw(surface)
         
