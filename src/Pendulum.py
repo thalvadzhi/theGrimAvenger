3114fcc43a25f51396686a7d5017b35d1df9c5fa
thalvadzhi <thalvadzhiev@gmail.com>
2014-08-30 15:33:32 +0300

    Pendulum upgrades

--- src/Pendulum.py
+++ src/Pendulum.py
@@ -1,45 +1,43 @@
-import pygame, sys
-from pygame.locals import *
 from math import sin, cos, radians
+from BasicShapes import Rectangle
 
-class Pendulum(pygame.sprite.Sprite):
+
+class Pendulum:
     def __init__(self, angle, swing_length, pivot):
-        pygame.sprite.Sprite.__init__(self)
         self.theta = angle
         self.dtheta = 0
         self.swing_length = swing_length
         self.pivot = pivot
         self.time = 0
-        self.rect = pygame.Rect(self.pivot[0]-self.swing_length*cos(radians(self.theta)),
-                                self.pivot[1]+self.swing_length*sin(radians(self.theta)),
-                                1, 1)
+        self.rect = Rectangle(1, 1,
+                              (int(self.pivot[0] - self.swing_length * cos(radians(self.theta))),
+                              int(self.pivot[1] + self.swing_length * sin(radians(self.theta)))))
 
     def recompute_angle(self):
         self.time += 1
         #modulates gravity
-        scaling = 2000.0 / (self.swing_length ** 2)
+        scaling = 2000 / (self.swing_length ** 2)
 
-        firstDDtheta = -sin(radians(self.theta)) * scaling
-        midDtheta = self.dtheta + firstDDtheta
-        midtheta = self.theta + (self.dtheta + midDtheta) / 2.0
+        first_d_d_theta = -sin(radians(self.theta)) * scaling
+        mid_d_theta = self.dtheta + first_d_d_theta
+        midtheta = self.theta + (self.dtheta + mid_d_theta) / 2.0
 
-        midDDtheta = -sin(radians(midtheta)) * scaling
-        midDtheta = self.dtheta + (firstDDtheta + midDDtheta) / 2
-        midtheta = self.theta + (self.dtheta + midDtheta) / 2
+        mid_d_d_theta = -sin(radians(midtheta)) * scaling
+        mid_d_theta = self.dtheta + (first_d_d_theta + mid_d_d_theta) / 2
+        midtheta = self.theta + (self.dtheta + mid_d_theta) / 2
 
-        midDDtheta = -sin(radians(midtheta)) * scaling
-        lastDtheta = midDtheta + midDDtheta
-        lasttheta = midtheta + (midDtheta + lastDtheta) / 2.0
+        mid_d_d_theta = -sin(radians(midtheta)) * scaling
+        last_d_theta = mid_d_theta + mid_d_d_theta
+        lasttheta = midtheta + (mid_d_theta + last_d_theta) / 2.0
 
-        lastDDtheta = -sin(radians(lasttheta)) * scaling
-        lastDtheta = midDtheta + (midDDtheta + lastDDtheta) / 2.0
-        lasttheta = midtheta + (midDtheta + lastDtheta) / 2.0
+        last_d_d_theta = -sin(radians(lasttheta)) * scaling
+        last_d_theta = mid_d_theta + (mid_d_d_theta + last_d_d_theta) / 2.0
+        lasttheta = midtheta + (mid_d_theta + last_d_theta) / 2.0
 
-        self.dtheta = lastDtheta
+        self.dtheta = last_d_theta
         self.theta = lasttheta
-        self.rect = pygame.Rect(self.pivot[0] -
-                                self.swing_length * sin(radians(self.theta)),
-                                self.pivot[1] +
-                                self.swing_length * cos(radians(self.theta)), 1, 1)
+        self.rect = Rectangle(1, 1,
+                              (int(self.pivot[0] - self.swing_length * sin(radians(self.theta))),
+                              int(self.pivot[1] + self.swing_length * cos(radians(self.theta)))))
 
 
