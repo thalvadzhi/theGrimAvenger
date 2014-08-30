6908f4cdc456252f6d1a5fef96d1692e02634429
thalvadzhi <thalvadzhiev@gmail.com>
2014-08-30 15:27:58 +0300

    CHANGES ARE OVER 9000

--- src/GraplingHook.py
+++ src/GraplingHook.py
@@ -3,20 +3,29 @@ import math
 from pygame.math import Vector2 as Vector
 import sys
 from Pendulum import Pendulum
-
+from BasicShapes import Rectangle
+from math import *
 
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 500))
 clock = pygame.time.Clock()
 
-class GraplingHook(pygame.sprite.Sprite):
+class GraplingHook():
     def __init__(self, x, y):
-        pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((60, 30))
-        self.image.fill((255, 0, 0))
-        self.rect = self.image.get_rect()
-        self.rect.center = [x, y]
+        self.image_master = pygame.image.load("graplinghook.png").convert_alpha()
+        self.image_master = pygame.transform.scale(self.image_master, (80, 40))
+        self.image = self.image_master
+
+        self.hook_image_master = pygame.image.load("hook.png").convert_alpha()
+        self.hook_image_master = pygame.transform.scale(self.hook_image_master, (40, 40))
+        self.hook_image = self.hook_image_master
+
+        self.hook_rect = Rectangle.get_rect(self.hook_image, (x, y))
+        self.rect = Rectangle.get_rect(self.image, (x, y))
+
+        self.hooker = Vector((self.hook_rect.x, self.hook_rect.y))
+
         self.aim = (x, y + 20)
         self.should_aim = True
         self.should_retract = False
@@ -27,15 +36,21 @@ class GraplingHook(pygame.sprite.Sprite):
         self.should_release = False
         self.time = 0
         self.distance_limit = 150
+        self.x = self.rect.x
+        self.y = self.rect.y
+        self.current_time = 0
+        self.last_time = 0
+        self.shooter = False
     
     def calculate_angle(self):
         self.angle = self.rope.angle_to(self.limit)
         self.bob = Pendulum(90 - self.angle, self.distance_limit, (self.hook.x, self.hook.y))
         
-    def retract(self):
-        screen.fill((255, 255, 255))
+    def retract(self, timer):
         if self.distance > self.distance_limit:
-            self.rect = self.rect.move([self.rope.x * 15, self.rope.y * 15])
+            self.rect.advance(self.rope.x * 700 * (timer / 1000), self.rope.y * 700 * (timer / 1000))
+            self.x += self.rope.x * 700 * (timer / 1000)
+            self.y += self.rope.y * 700 * (timer / 1000)
             self.calculate_rope()
         else:
             self.swing()
@@ -47,24 +62,56 @@ class GraplingHook(pygame.sprite.Sprite):
         self.rope = self.hook - self.player
         self.rope = self.rope.normalize()
 
-    
+
     def swing(self):
-        self.bob.recompute_angle()
-        self.step = self.bob.dtheta
-        self.rope.rotate_ip(self.step)
-        self.rect.center = self.bob.rect.center
+        self.current_time = pygame.time.get_ticks()
+        if self.current_time - self.last_time >= 17:
+            self.bob.recompute_angle()
+            self.step = self.bob.dtheta
+            self.rope = self.rope.rotate(self.step)
+            self.rect = Rectangle.get_rect(self.image, self.bob.rect.center)
+            self.x = self.rect.x
+            self.y = self.rect.y
 
+            self.last_time = self.current_time
 
-    def functionality(self, event):
+    def shoot(self, timer):
+        distance = sqrt((self.hook_rect.center[0] - self.aim[0]) ** 2 + (self.hook_rect.center[1] - self.aim[1]) ** 2)
+        if int(distance) > 3:
+            self.hook_rect.advance(self.rope.x * 700 * (timer / 1000), self.rope.y * 700 * (timer / 1000))
+            self.hooker = Vector((self.hook_rect.x, self.hook_rect.y))
+            print(distance)
+        else:
+            self.shooter = False
+
+    def update(self, screen, timer):
+        angle = Vector(self.aim) - Vector(self.rect.center)
+        ang = angle.normalize()
+        self.image = pygame.transform.rotate(self.image_master, angle.angle_to(Vector(1, 0)))
+        self.hook_image = pygame.transform.rotate(self.hook_image_master, angle.angle_to(Vector(1, 0)))
+        self.rect = Rectangle.get_rect(self.image, self.rect.center)
+
+        self.x = self.rect.x
+        self.y = self.rect.y
+        if self.shooter:
+            self.shoot(timer)
+        elif not self.should_retract:
+            #60 is the average of the widths of the two images
+            self.hook_rect = Rectangle.get_rect(self.hook_image, (self.rect.center[0] + ang.x * 60, self.rect.center[1] + ang.y * 60))
+        if self.should_retract and not self.shooter:
+            self.retract(timer)
+        if self.should_release:
+            self.release(timer)
+        self.draw(screen)
+
+    def functionality(self, event, screen):
         if event.type == pygame.MOUSEMOTION and self.should_aim:
-                screen.fill((255, 255, 255))
                 self.aim = pygame.mouse.get_pos()
-                self.draw()
-            
+
         if event.type == pygame.MOUSEBUTTONUP and self.should_aim:
             self.should_aim = False
-            self.draw()
             self.should_retract = True
+            self.shooter = True
             self.calculate_rope()
             self.calculate_angle()
             
@@ -73,32 +120,32 @@ class GraplingHook(pygame.sprite.Sprite):
                 self.should_release = True
                 self.should_retract = False
                 
-    def release(self):
-        screen.fill((255, 255, 255))
+    def release(self, timer):
         self.calculate_rope()
-        self.rect = self.rect.move([-self.rope.x * 8 - (math.sin(self.bob.theta) * int(self.bob.dtheta)),
-                                    -self.rope.y * 10 + self.rope.y * (15 - self.time)])
-        self.time += 0.7
+
+        self.rect.advance((- self.rope.x * 10 - (math.sin(self.bob.theta) * int(self.bob.dtheta))) * 15 * (timer / 1000),
+                       (- self.rope.y * 10 + self.rope.y * (30 - self.time)) * 15 * (timer / 1000))
+        self.time += 0.8
     
-    def draw(self):
-        if self.should_retract:
-            self.retract()
+    def draw(self, screen):
         pygame.draw.line(screen, (0, 255, 0), self.rect.center, self.aim)
-        if self.should_release:
-            self.release()
-        
-        
-graple = GraplingHook(100, 450)  
-graple.x = 50
-graple.y = 20
+        screen.blit(self.image, (self.x, self.y))
+        screen.blit(self.hook_image, (self.hook_rect.x, self.hook_rect.y))
+
+graple = GraplingHook(100, 450)
+
 #should_retract = False
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        graple.functionality(event)
-    graple.draw()
-    screen.blit(graple.image, graple.rect)
-    
+        graple.functionality(event, screen)
+
+    time = clock.tick(120)
+
+    screen.fill((255, 255, 255))
+    graple.update(screen, time)
+    graple.draw(screen)
+
+
     pygame.display.update()
-    clock.tick(60)
