de26e1c0f380d9c63ebd6966152b0bf54a4a36c3
thalvadzhi <thalvadzhiev@gmail.com>
2014-09-03 22:54:37 +0300

    fixes

--- src/Batarangs.py
+++ src/Batarangs.py
@@ -1,28 +1,23 @@
-import math
-import sys
 import pygame
-import Camera
-#import Environment
-from pygame.math import Vector2 as Vector
+from Vec2D import Vec2d as Vector
 from BasicShapes import Rectangle
 from pixelperfect import get_hitmask
 
 class Batarang():
     def __init__(self, x, y):
         #x and y are the coordinates of player's hand
-        self.width = 60
-        self.height = 30
-        self.image_master = pygame.image.load("batarang.png").convert_alpha()
+        self.width = 40
+        self.height = 20
+        self.image_master = pygame.image.load("batarang2.png").convert_alpha()
         self.image_master = pygame.transform.scale(self.image_master, (self.width, self.height))
         self.image = self.image_master
         self.rect_center = (x, y)
         self.rect = Rectangle.get_rect(self.image, self.rect_center)
-
+        self.hitmask = get_hitmask(self.rect, self.image, 0)
         #coordinates of topleft vertex
         self.x = x - self.width // 2
         self.y = y - self.height // 2
 
-        self.mask = get_hitmask(self, 0)
 
         self.rotation = 0
         self.gravity = 0
@@ -34,27 +29,27 @@ class Batarang():
     def rotate(self, timer):
         self.image = pygame.transform.rotate(self.image_master, self.rotation * (1000 / timer))
         self.rect = Rectangle.get_rect(self.image, self.rect_center)
-        self.mask = get_hitmask(self, 0)
+
+        self.hitmask = get_hitmask(self.rect, self.image, 0)
+
         self.rotation += self.step
         if self.rotation > 360:
             self.rotation = self.step
         if self.frames >= 25:
             self.step -= 0.7
 
-
     def move(self, timer):
-        #IMPLEMENT GRAVITY
         speed = 25 * 60 * (timer / 1000)
         self.x += self.direction.x * speed
         self.y += self.direction.y * speed
 
         old_center = self.rect_center
-
+        self.rect.center = old_center
         self.rect_center = (old_center[0] + self.direction.x * speed, old_center[1] + self.direction.y * speed)
+        self.hitmask = get_hitmask(self.rect, self.image, 0)
 
         self.rotate(timer)
-        
-    
+
     def direct(self, mouse_x, mouse_y):
         #get coordinates of player hand
         #get coordinates of mouse
@@ -62,7 +57,7 @@ class Batarang():
         self.direction = Vector(mouse_x - self.x, mouse_y - self.y)
         self.direction = self.direction.normalize()
 
-    def control(self):
+    def take_action(self):
         mouse_position = pygame.mouse.get_pos()
         self.direct(mouse_position[0], mouse_position[1])
         self.should_fly = True
@@ -71,9 +66,14 @@ class Batarang():
         if self.should_fly:
             self.move(timer)
             self.rotate(timer)
+            #self.hitmask = get_hitmask(self.rect, self.image, 0)
+        #self.rect = self.generate_rect(image, self.rect.center)
 
     def draw(self, surface, camera):
-        #to be moved in Render module
-        surface.blit(self.image, camera.apply_to_tuple((self.x, self.y)))
-        self.rect.draw(surface)
-        
+        surface.blit(self.image, camera.apply((self.x, self.y)))
+
+    def reposition(self, coordinates):
+        self.rect.center = Vector(coordinates)
+        self.x = coordinates[0]
+        self.y = coordinates[1]
+
