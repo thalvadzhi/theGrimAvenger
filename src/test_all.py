7eda607b379487bb122dcd64e3558cbe66d8f77f
thalvadzhi <thalvadzhiev@gmail.com>
2014-09-04 05:14:29 +0300

    all tests are imported in here

---
+++ src/test_all.py
@@ -0,0 +1,13 @@
+import unittest
+
+from button_tests import *
+from grapling_hook_test import *
+from test_camera import *
+from test_pendulum import *
+from batarang_test import *
+from environment_test import *
+from pixel_perfect_test import *
+
+
+if __name__ == '__main__':
+    unittest.main()
