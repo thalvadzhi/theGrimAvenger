2c0c2cb52bc008576d5809f9504617b4dab354eb
thalvadzhi <thalvadzhiev@gmail.com>
2014-09-04 18:21:16 +0300

    pep8 compliant now

--- src/pixel_perfect_test.py
+++ src/pixel_perfect_test.py
@@ -9,18 +9,19 @@ class PendulumTest(unittest.TestCase):
     def test_get_hitmask(self):
         rect = Rectangle(2, 2, (1, 1))
         image = pygame.Surface((2, 2))
-        self.assertEqual(get_hitmask(rect, image,0), [[True, True], [True, True]])
+        self.assertEqual(get_hitmask(rect, image, 0),
+                         [[True, True], [True, True]])
 
     def test_collide(self):
         rect1 = Rectangle(2, 2, (1, 1))
         rect2 = Rectangle(2, 2, (1, 2))
         image = pygame.Surface((2, 2))
-        self.assertEqual(collide(rect1, [[True, True], [True, True]], rect2, [[True, True], [True, True]]), True)
+        self.assertEqual(collide(rect1, [[True, True], [True, True]],
+                                 rect2, [[True, True], [True, True]]), True)
         rect2 = Rectangle(2, 2, (1, 15))
-        self.assertEqual(collide(rect1, [[True, True], [True, True]], rect2, [[True, True], [True, True]]), False)
-        rect2 = Rectangle(0, 0, (0, 0))
+        self.assertEqual(collide(rect1, [[True, True], [True, True]],
+                                 rect2, [[True, True], [True, True]]), False)
 
 
 if __name__ == '__main__':
     unittest.main()
-
