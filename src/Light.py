081e94d06a5d288b9d7f67612af0cc1537a22639
Teodor Halvadzhiev <thalvadzhiev@gmail.com>
2015-06-07 02:03:57 +0300

    changed drawing routine so that multiple lights can exist at the same time

--- src/Light.py
+++ src/Light.py
@@ -1,28 +1,44 @@
 import pygame
+import math
+from Environment import Block
 from light_cast_v3 import LightSource, Point
 from gradients import radial
 from pygame import gfxdraw
+import pygame
 class Light:
-    def __init__(self, x, y, obstacles, screen_x, screen_y):
+
+    SHADOW_SURFACE = 0
+    LIGHT_SURFACE = 0
+
+    @classmethod
+    def set_up_surfaces(cls, width, height):
+        cls.SHADOW_SURFACE = pygame.Surface((width, height), pygame.HWSURFACE).convert_alpha()
+        cls.LIGHT_SURFACE = pygame.Surface((width, height), pygame.HWSURFACE).convert_alpha()
+
+    def __init__(self, x, y, radius, screen_x, screen_y, obstacles=[]):
+        #expect that obstacles are blocks
         self.x = x
         self.y = y
+        self.radius = radius
         self.obstacles = obstacles
         self.screen_x = screen_x
         self.screen_y = screen_y
         self.light_image = pygame.Surface((self.screen_x, self.screen_y), pygame.HWSURFACE).convert_alpha()
         self.light_surface = pygame.Surface((self.screen_x, self.screen_y), pygame.HWSURFACE).convert_alpha()
-        self.light_texture = radial(400, (255, 255, 255, 255), (0, 0, 0, 255))
+        self.light_texture = radial(radius, (255, 255, 255, 255), (0, 0, 0, 255))
         self.bg_surface = pygame.Surface((800, 600), pygame.HWSURFACE)
         self.lightSource = LightSource(self.x, self.y, self.generate_points_from_rects())
         self.visibility = self.lightSource.cast()
 
-    def generate_points_from_rects(self):
+
+    def generate_points_from_rects(self, ):
         all_points = []
         for obstacle in self.obstacles:
             current_points = []
-            for vertex in obstacle.vertices:
-                current_points.append(Point(vertex.x, vertex.y))
-            all_points.append(current_points)
+            if isinstance(obstacle, Block):
+                for vertex in obstacle.rect.vertices:
+                    current_points.append(Point(vertex.x, vertex.y))
+                all_points.append(current_points)
         #also add dimensions of screen
         all_points.append([Point(0, 0), Point(self.screen_x, 0),
                            Point(self.screen_x, self.screen_y), Point(0, self.screen_y)])
@@ -35,55 +51,49 @@ class Light:
         self.x = x
         self.y = y
         self.lightSource.update_light_source_position(self.x, self.y)
+        self.update()
 
     def update_obstacles(self, obstacles):
         self.obstacles = obstacles
         self.lightSource.update_obstacles(self.generate_points_from_rects())
+        self.update()
 
-    def draw(self, surface, camera):
+    # def draw(self, surface, camera):
+    #     #light surface is the visibility polygon
+    #     #bg image is the gradient
+    #     self.light_image.fill((255, 255, 255, 255))
+    #     pygame.draw.polygon(self.light_image, (0, 0, 0, 0), camera.apply(self.visibility))
+    #     self.light_surface.fill((0, 0, 0, 255))
+    #     self.light_surface.blit(self.light_image, (0, 0), None, pygame.BLEND_RGBA_MIN)
+    #
+    #     self.bg_surface.fill((0, 0, 0))
+    #     self.bg_surface.blit(self.light_texture, (self.x - 400, self.y - 400), None, pygame.BLEND_RGB_MAX)
+    #
+    #     #surface.blit(self.bg_surface, (0, 0))
+    #     surface.blit(self.light_surface, (0, 0))
+    #     gfxdraw.aapolygon(surface, camera.apply(self.visibility), (0, 0, 0))
+
+    @classmethod
+    def nullify_light(cls):
+        Light.LIGHT_SURFACE.fill((0, 0, 0))
+
+    @classmethod
+    def nullify_shadow(cls):
+        Light.SHADOW_SURFACE.fill((0, 0, 0, 255))
+
+    def draw_shadow(self, camera):
         self.light_image.fill((255, 255, 255, 255))
         pygame.draw.polygon(self.light_image, (0, 0, 0, 0), camera.apply(self.visibility))
-        self.light_surface.fill((0, 0, 0, 255))
-        self.light_surface.blit(self.light_image, (0, 0), None, pygame.BLEND_RGBA_MIN)
-        surface.blit(self.light_surface, (0, 0))
+        Light.SHADOW_SURFACE.blit(self.light_image, (0, 0), None, pygame.BLEND_RGBA_MIN)
 
+    def draw_light(self, camera):
+        Light.LIGHT_SURFACE.blit(self.light_texture, camera.apply((self.x - 400, self.y - 400)), None, pygame.BLEND_RGB_MAX)
 
+    @classmethod
+    def draw_everything(cls, surface):
+        surface.blit(Light.LIGHT_SURFACE, (0, 0))
+        surface.blit(Light.SHADOW_SURFACE, (0, 0))
 
-        self.bg_surface.fill((0, 0, 0))
-        self.bg_surface.blit(self.light_texture, (self.x - 400, self.y - 400), None, pygame.BLEND_RGB_MAX)
-
-        surface.blit(self.bg_surface, (0, 0))
-        surface.blit(self.light_surface, (0, 0))
-        gfxdraw.aapolygon(surface, camera.apply(self.visibility), (0, 0, 0))
-
-#        pygame.draw.aalines(surface, (0, 0, 0), True, camera.apply(self.visibility))
-        # ####EXPERIMENTAL
-        # #get mask
-        # mask = pygame.Surface((800, 600), depth=8)
-        # pygame.draw.polygon(mask, 255, self.visibility, 0)
-        # #get texture
-        # texture = radial(300, (50, 50, 50, 0), (50, 50, 50, 150))
-        # texture = self.tile_texture(texture, (800, 600))
-        # self.stamp(surface, texture, mask)
-
-
-    #experimental
-    def apply_alpha(self, texture, mask):
-        texture = texture.convert_alpha()
-        target = pygame.surfarray.pixels_alpha(texture)
-        target[:] = pygame.surfarray.array2d(mask)
-        # surfarray objets usually lock the Surface.
-        # it is a good idea to dispose of them explicitly
-        # as soon as the work is done.
-        del target
-        return texture
-
-    def stamp(self, image, texture, mask):
-        image.blit(self.apply_alpha(texture, mask), (0,0))
-
-    def tile_texture(self, texture, size):
-        result = pygame.Surface(size, depth=32)
-        for x in range(0, size[0], texture.get_width()):
-            for y in range(0, size[1], texture.get_height()):
-                result.blit(texture, (x, y))
-        return result
+    def collide(self, position):
+       #for moving purposes
+       return math.sqrt((self.x - position[0]) ** 2 + (self.y - position[1]) ** 2) <= 30
