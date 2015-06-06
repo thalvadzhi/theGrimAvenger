7959e46a32450a67c3fd042a59c89c4d3a08b6bb
Teodor Halvadzhiev <thalvadzhiev@gmail.com>
2015-06-07 02:02:26 +0300

    added file for constants

---
+++ src/Constants.py
@@ -0,0 +1,17 @@
+OBJECT_BLOCK = "block"
+OBJECT_SAW_BLOCK = "sawblock"
+OBJECT_LIGHT = "light"
+OBJECT = "object"
+EXPAND_FIELD_UP = "expand up"
+EXPAND_FIELD_RIGHT = "expand right"
+RETRACT_FIELD_DOWN = "retract down"
+RETRACT_FIELD_LEFT = "retract left"
+DECODING_FAILURE = "decode failure"
+NO_OBJECT_SELECTED = "no object selected"
+TAG_WALL = "wall"
+TAG_GROUND = "ground"
+HEIGHT = 600
+WIDTH = 800
+FPS = 60
+
+CONSTANTS = [WIDTH, HEIGHT, FPS]
